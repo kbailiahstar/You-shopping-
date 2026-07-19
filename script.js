f40dcc7bb3518c4358a0a1e9c138840c5671741e
kbailiahstar <kbailaih@gmail.com>
2026-07-19 11:31:57 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const buttons = document.querySelectorAll("button");
+let cart = 0;
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    alert("Product added to cart!");
-  });
+function addToCart(productName) {
+    cart++;
+    alert(productName + " added to cart!\n\nItems in cart: " + cart);
+}
+
+document.querySelectorAll(".card button").forEach((button) => {
+    button.addEventListener("click", function () {
+        const product = this.parentElement.querySelector("h3").innerText;
+        addToCart(product);
+    });
 });
